020911a5326b33ede26b8b1da3d475ce741d3434
“甘肃” <1374646309@qq.com>
2021-03-18 18:10:34 +0800

    地图定位

--- src/views/Dashboard/Map.vue
+++ src/views/Dashboard/Map.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="home">
-  <baidu-map class="map" :center="{lng: 116.404, lat: 39.915}" :zoom="15">
-    <bm-marker :position="{lng: 116.404, lat: 39.915}" :dragging="true" animation="BMAP_ANIMATION_BOUNCE">
-      <bm-label content="我爱北京天安门" :labelStyle="{color: 'red', fontSize : '24px'}" :offset="{width: -35, height: 30}"/>
+  <baidu-map class="map" :center="{lng: 117.192, lat: 31.771}" :zoom="15">
+    <bm-marker :position="{lng: 117.192, lat: 31.771}" :dragging="true" animation="BMAP_ANIMATION_BOUNCE">
+      <bm-label content="安徽大学" :labelStyle="{color: 'red', fontSize : '24px'}" :offset="{width: -35, height: 30}"/>
     </bm-marker>
   </baidu-map>
   </div>
