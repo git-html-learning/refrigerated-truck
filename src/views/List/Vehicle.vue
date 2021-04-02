68cf5220752d28938d3d73aab7245cafd839865d
“甘肃” <1374646309@qq.com>
2021-04-02 11:22:37 +0800

    车辆基础信息管理优化

--- src/views/List/Vehicle.vue
+++ src/views/List/Vehicle.vue
@@ -13,7 +13,7 @@
               <a-button type="primary" @click="search">查询</a-button>
             </a-form-model-item>
             <a-form-model-item>
-              <a-button type="primary">重置</a-button>
+              <a-button type="primary" @click="reset">重置</a-button>
             </a-form-model-item>
 
             <a-button
@@ -36,22 +36,34 @@
             @click="more(item)"
           >
             <a-card hoverable class="vehicle">
-              <a-card-meta :title="item.number">
+              <a-card-meta :title="item.productName" >
                 <a-avatar :size="64" slot="avatar" style="background: #1980ff">
                   <!-- <i class="iconfont icon-cangku"></i> -->
                   <img src="../../static/icon/车辆.svg" >
                 </a-avatar>
               </a-card-meta>
-              <p style="padding-left: 64px">
+              <!-- <p style="padding-left: 64px">
                 司机:
-                <span style="font-weight: 600; font-size: 22px">
+                <span style="font-weight: 600; font-size: 20px">
                   {{ item.driver }}
                 </span>
               </p>
               <p style="padding-left: 80px">
                 目的地:
-                <span style="font-weight: 600; font-size: 22px">
+                <span style="font-weight: 600; font-size: 20px">
                   {{ item.destination }}
+                </span>
+              </p> -->
+              <p style="padding-left: 64px">
+                温湿度传感器数量:
+                <span style="font-weight: 600; font-size: 20px">
+                  {{ item.extraInfo.tempAndHumi.Value}}
+                </span>
+              </p>
+              <p style="padding-left: 80px">
+                胎温胎压传感器数量:
+                <span style="font-weight: 600; font-size: 20px">
+                  {{ item.extraInfo.tireTempPress.Value }}
                 </span>
               </p>
             </a-card>
@@ -63,133 +75,165 @@
       </div>
     </div>
 
-    <!-- <a-modal
+    <a-modal
       title="车辆注册"
       :visible="visible"
       @ok="handleOk"
       @cancel="handleCancel"
     >
-      <a-form-model :label-col="labelCol" :wrapper-col="wrapperCol">
+      <a-form-model :label-col="{ span: 6}" :wrapper-col="{ span: 12 }">
         <a-form-model-item label="车牌号">
-          <a-input v-model="vehicleList.number" />
+          <a-input v-model="productname" />
         </a-form-model-item>
-        <a-form-model-item label="司机">
+        <!-- <a-form-model-item label="司机">
           <a-input v-model="vehicleList.driver" />
         </a-form-model-item>
         <a-form-model-item label="目的地">
           <a-input v-model="vehicleList.destination" />
-        </a-form-model-item>
+        </a-form-model-item> -->
       </a-form-model>
-    </a-modal> -->
+    </a-modal>
 
-    <div v-show="showInfo == '2'">
+    <!-- <div v-show="showInfo == '2'">
       <div class="home-item1">
-        <a-form-model :label-col="labelCol" :wrapper-col="wrapperCol">
-          <a-form-model-item label="车牌号">
+        <a-form-model :label-col="{ span: 8 }" >
+          <a-form-model-item label="车牌号" :wrapper-col="{ span: 6 }">
             <a-input v-model="vehicleList.number" />
           </a-form-model-item>
-          <a-form-model-item label="司机">
+          <a-form-model-item label="司机" :wrapper-col="{ span: 6 }">
             <a-input v-model="vehicleList.driver" />
           </a-form-model-item>
-          <a-form-model-item label="目的地">
+          <a-form-model-item label="目的地" :wrapper-col="{ span: 6 }">
             <a-input v-model="vehicleList.destination" />
           </a-form-model-item>
         </a-form-model>
-     <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
+     <a-form-item :wrapper-col="{ span: 14, offset: 8 }">
       <a-button type="primary" @click="handleOk">注册</a-button>
       <a-button style="margin-left: 10px" @click="handleCancel">取消</a-button>
     </a-form-item>
         
       </div>
-    </div>
+    </div> -->
 
 
   </div>
 </template>
 <script>
+// import {
+//   product,
+//   getdeviceList,
+//   getdeviceData,
+//   onlinestatus,
+// } from "@/api/data";
 import {
   product,
-  getdeviceList,
-  getdeviceData,
-  onlinestatus,
-} from "@/api/data";
+  register,
+} from "@/api/interface";
+
 export default {
   data() {
     return {
       showInfo: "1", //切换页面
       searchNum: "", //车牌号搜索
       visible: false, //对话框显示
-      labelCol: { span: 4 }, //lable标签布局
-      wrapperCol: { span: 14 }, //输入控件布局
-      vehicleList: [
-        {
-          number: "皖A11111",
-          driver: "俞大",
-          destination: "上海",
-        },
-        {
-          number: "皖A22222",
-          driver: "陈二",
-          destination: "浙江",
-        },
-        {
-          number: "皖A33333",
-          driver: "张三",
-          destination: "福建",
-        },
-        {
-          number: "皖A44444",
-          driver: "李五",
-          destination: "广州",
-        },
-        {
-          number: "皖A55555",
-          driver: "汪鸽",
-          destination: "武汉",
-        },
-      ],
+      productname:"",  //注册车辆号码
+
+      vehicleList:[],
+      // vehicleList: [
+      //   {
+      //     number: "皖A11111",
+      //     driver: "俞大",
+      //     destination: "上海",
+      //   },
+      //   {
+      //     number: "皖A22222",
+      //     driver: "陈二",
+      //     destination: "浙江",
+      //   },
+      //   {
+      //     number: "皖A33333",
+      //     driver: "张三",
+      //     destination: "福建",
+      //   },
+      //   {
+      //     number: "皖A44444",
+      //     driver: "李五",
+      //     destination: "广州",
+      //   },
+      //   {
+      //     number: "皖A55555",
+      //     driver: "汪鸽",
+      //     destination: "武汉",
+      //   },
+      // ],
       testV: [], //注册测试
     };
   },
+
   created(){
     this.getproduct()
   },
+
   methods: {
     async getproduct(){   //获取产品信息
       const res = await product();
-      console.log(res);
+      // console.log(res);
+      for(var i=0;i<res.data.productInfo.length;i++){
+        if(res.data.productInfo[i].typeIdentify=="tylcc"){
+          // console.log(res.data.productInfo[i]);
+          this.vehicleList.push(res.data.productInfo[i])
+        }
+      }
+      console.log(this.vehicleList);
     },
+
+
     more(data) {   //点击卡片
       console.log(data);
     },
-    search() {
+
+    search() {   //查询车辆
       var arr = [];
       for (var i = 0; i < this.vehicleList.length; i++) {
-        if (this.vehicleList[i].number.indexOf(this.searchNum) >= 0) {
+        if (this.vehicleList[i].productName.indexOf(this.searchNum) >= 0) {
           arr.push(this.vehicleList[i]);
         }
       }
       this.vehicleList = arr;
     },
+    reset(){   //重置
+      this.vehicleList=[]
+      this.searchNum=""
+      this.getproduct()
+    },
+
     add() {
-      this.showInfo = "2";
+      // this.showInfo = "2";
+      this.visible=true
+      this.productname=""
     },
-    handleOk() {
+    
+    async handleOk() {   //注册
       var _this = this;
-      // _this.visible = false;
-      _this.showInfo="1"
-      var testV = new Object();
-      _this.testV.number = _this.vehicleList.number;
-      _this.testV.driver = _this.vehicleList.driver;
-      _this.testV.destination = _this.vehicleList.destination;
-      console.log(_this.testV);
-      // _this.vehicleList.push(_this.testV)
-      // console.log(_this.vehicleList);
+      _this.visible = false;
+      // _this.showInfo="1"
+      // console.log(_this.productname);
+      const res=await register({
+        productName:this.productname
+      })
+      // console.log(res);
+      if(res.code==200){
+        _this.vehicleList=[]
+        _this.$message.success("注册成功!")
+        _this.getproduct()
+      }      
     },
+
     handleCancel() {
-      // this.visible = false;
-      this.showInfo = "1";
-      console.log("取消");
+      this.visible = false;
+      // this.showInfo = "1";
+      this.$message.info("取消注册")
+      // console.log("取消");
     },
 
   },
@@ -237,4 +281,5 @@ export default {
   box-shadow: 5px 5px 5px #e2e1e1;
   border-radius: 10px;
 }
+
 </style>
