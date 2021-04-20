88a9af1beec9359435f3b1dfa99d4973b1e7528a
“甘肃” <1374646309@qq.com>
2021-04-20 20:23:45 +0800

    地图页优化

--- src/views/Dashboard/Map.vue
+++ src/views/Dashboard/Map.vue
@@ -1,42 +1,115 @@
 <template>
-  <div class="home">
-    <baidu-map
-      class="map"
-      :center="{ lng: lg, lat: lt }"
-      :zoom="12"
-      :scroll-wheel-zoom="true"
-      
-    >
-      <bm-map-type :map-types="['BMAP_NORMAL_MAP', 'BMAP_HYBRID_MAP']" anchor="BMAP_ANCHOR_TOP_LEFT"></bm-map-type>
-      <bm-marker
-        v-for="(item, index) in markers"
-        :key="index"
-        :position="{ lng: markers[index].lng, lat: markers[index].lat }"
-        @click="lookDetail(item)"
-        :dragging="false"
-        animation="BMAP_ANIMATION_BOUNCE"
-        :icon="{url: require('../../static/pic/卡车.png'), size: {width: 50, height: 50}}"
+  <div>
+    <div style="height: 40px; background: rgb(53, 62, 67)">
+      <a-row>
+        <a-col :span="2">
+          <div
+            style="
+              height: 40px;
+              color: white;
+              line-height: 32px;
+              padding-left: 30px;
+            "
+          >
+            全部车辆 &nbsp;<span style="color: white; font-size: 24px">3</span>
+          </div>
+        </a-col>
+        <a-col :span="2">
+          <div
+            style="
+              height: 40px;
+              color: white;
+              line-height: 32px;
+              padding-left: 30px;
+            "
+          >
+            行驶 &nbsp;
+            <span style="color: rgb(83, 189, 119); font-size: 24px">1</span>
+          </div>
+        </a-col>
+        <a-col :span="2">
+          <div
+            style="
+              height: 40px;
+              color: white;
+              line-height: 32px;
+              padding-left: 30px;
+            "
+          >
+            静止 &nbsp;
+            <span style="color: rgb(251, 178, 7); font-size: 24px">1</span>
+          </div>
+        </a-col>
+        <a-col :span="2">
+          <div
+            style="
+              height: 40px;
+              color: white;
+              line-height: 32px;
+              padding-left: 30px;
+            "
+          >
+            离线 &nbsp; <span style="color: red; font-size: 24px">1</span>
+          </div>
+        </a-col>
+        <a-col :span="2">
+          <div
+            style="
+              height: 40px;
+              color: white;
+              line-height: 32px;
+              padding-left: 30px;
+            "
+          >
+            其他 &nbsp; <span style="color: grey; font-size: 24px">0</span>
+          </div>
+        </a-col>
+      </a-row>
+    </div>
+    <div class="home">
+      <baidu-map
+        class="map"
+        :center="{ lng: lg, lat: lt }"
+        :zoom="12"
+        :scroll-wheel-zoom="true"
       >
-        <!-- :icon="{url: 'http://pic95.huitu.com/res/20170428/851091_20170428215847978040_1.jpg', size: {width: 62, height: 62}}" -->
-        <!-- <bm-label content="安徽大学" :labelStyle="{color: 'red', fontSize : '24px'}" :offset="{width: -35, height: 30}"/> -->
-      </bm-marker>
-      <bm-info-window
-        :title="title"
-        :position="position"
-        :show="showFlag"
-        @close="infoWindowClose()"
-        @open="infoWindowOpen()"
-      >
-        <div>
-          <p>{{"更新时间：&nbsp;&nbsp;"+time}}</p>
-          <p>{{"当前速度：&nbsp;&nbsp;"+speed}}</p>
-        </div>       
-      </bm-info-window>
-      <bm-overview-map
-        anchor="BMAP_ANCHOR_BOTTOM_RIGHT"
-        :isOpen="true"
-      ></bm-overview-map>
-    </baidu-map>
+        <bm-map-type
+          :map-types="['BMAP_NORMAL_MAP', 'BMAP_HYBRID_MAP']"
+          anchor="BMAP_ANCHOR_TOP_LEFT"
+        ></bm-map-type>
+        <bm-marker
+          v-for="(item, index) in markers"
+          :key="index"
+          :position="{ lng: markers[index].lng, lat: markers[index].lat }"
+          @click="lookDetail(item)"
+          :dragging="false"
+          animation="BMAP_ANIMATION_BOUNCE"
+          :icon="{
+            url: require('../../static/pic/卡车.png'),
+            size: { width: 50, height: 50 },
+          }"
+        >
+          <!-- :icon="{url: 'http://pic95.huitu.com/res/20170428/851091_20170428215847978040_1.jpg', size: {width: 62, height: 62}}" -->
+          <!-- <bm-label content="安徽大学" :labelStyle="{color: 'red', fontSize : '24px'}" :offset="{width: -35, height: 30}"/> -->
+        </bm-marker>
+        <bm-info-window
+          :title="title"
+          :position="position"
+          :show="showFlag"
+          @close="infoWindowClose()"
+          @open="infoWindowOpen()"
+        >
+          <div>
+            <p>{{ "更新时间：&nbsp;&nbsp;" + time }}</p>
+            <p>{{ "当前速度：&nbsp;&nbsp;" + speed }}</p>
+          </div>
+        </bm-info-window>
+        <bm-overview-map
+          anchor="BMAP_ANCHOR_BOTTOM_RIGHT"
+          :isOpen="true"
+        ></bm-overview-map>
+      </baidu-map>
+    </div>
   </div>
 </template>
 
@@ -49,34 +122,34 @@ export default {
   data() {
     return {
       lg: "117.192",
-      lt: "31.671",     //地图中心
-      title:"",
+      lt: "31.671", //地图中心
+      title: "",
       position: {},
       showFlag: false,
-      time:"",
-      speed:"",
+      time: "",
+      speed: "",
 
       markers: [
         {
           lng: 117.192,
           lat: 31.771,
           number: "皖A11111",
-          updateTime:"2021-03-24 13:14",
-          speed:"78Km/h"
+          updateTime: "2021-03-24 13:14",
+          speed: "78Km/h",
         },
         {
           lng: 117.092,
           lat: 31.671,
           number: "皖A22222",
-          updateTime:"2021-03-24 13:16",
-          speed:"80Km/h"
+          updateTime: "2021-03-24 13:16",
+          speed: "80Km/h",
         },
         {
           lng: 117.002,
           lat: 31.631,
           number: "皖A33333",
-          updateTime:"2021-03-24 13:24",
-          speed:"79Km/h"
+          updateTime: "2021-03-24 13:24",
+          speed: "79Km/h",
         },
       ],
       info: {},
@@ -86,14 +159,14 @@ export default {
   methods: {
     lookDetail(data) {
       console.log(data);
-      this.title=data.number
+      this.title = data.number;
       this.showFlag = true;
       this.position = {
         lng: data.lng,
         lat: data.lat,
       };
-      this.time=data.updateTime
-      this.speed=data.speed
+      this.time = data.updateTime;
+      this.speed = data.speed;
     },
     // 关闭弹窗
     infoWindowClose() {
@@ -108,6 +181,7 @@ export default {
 </script>
 
 <style lang="less" scoped>
+
 .home {
   margin: 10px auto;
   width: 100%;
