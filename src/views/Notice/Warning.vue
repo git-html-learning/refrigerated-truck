dc234024f656b687127338d4139fd95ac8d922d5
“甘肃” <1374646309@qq.com>
2021-04-23 10:50:55 +0800

    警报表格

--- src/views/Notice/Warning.vue
+++ src/views/Notice/Warning.vue
@@ -11,23 +11,32 @@
 
         <div class="warningdiagram" id="warningdiagram"></div>
       </div>
-      
+
       <div class="home-item-bottom">
         <!-- <p style="font-size: 20px; padding: 0px 0px 0px 10px; align: left">类别</p> -->
-        <a-tabs  default-active-key="1" size="large">
+        <a-tabs default-active-key="1" size="large">
           <a-tab-pane key="1" tab="全部">
             <div>
-              <a-table 
-              :columns="totalColumns" 
-              :data-source="totalData"
-              bordered 
-              :pagination="paginationOpt"
+              <a-table
+                :columns="totalColumns"
+                :data-source="totalData"
+                bordered
+                :pagination="paginationOpt"
               >
-              <a slot="number" slot-scope="text">{{ text }}</a>
+                <a slot="number" slot-scope="text">{{ text }}</a>
+                <span slot="tags" slot-scope="tags">
+                  <a-tag
+                    v-for="tag in tags"
+                    :key="tag"
+                    :color="tag === '报警' ? 'red' : 'purple'"
+                  >
+                    {{ tag.toUpperCase() }}
+                  </a-tag>
+                </span>
               </a-table>
             </div>
           </a-tab-pane>
-          
+
           <a-tab-pane key="2" tab="胎温">
             <div>
               <a-table
@@ -36,7 +45,16 @@
                 bordered
                 :pagination="paginationOpt"
               >
-              <a slot="number" slot-scope="text">{{ text }}</a>
+                <a slot="number" slot-scope="text">{{ text }}</a>
+                <span slot="tags" slot-scope="tags">
+                  <a-tag
+                    v-for="tag in tags"
+                    :key="tag"
+                    :color="tag === '报警' ? 'red' : 'purple'"
+                  >
+                    {{ tag.toUpperCase() }}
+                  </a-tag>
+                </span>
               </a-table>
             </div>
           </a-tab-pane>
@@ -48,7 +66,16 @@
                 bordered
                 :pagination="paginationOpt"
               >
-              <a slot="number" slot-scope="text">{{ text }}</a>
+                <a slot="number" slot-scope="text">{{ text }}</a>
+                <span slot="tags" slot-scope="tags">
+                  <a-tag
+                    v-for="tag in tags"
+                    :key="tag"
+                    :color="tag === '报警' ? 'red' : 'purple'"
+                  >
+                    {{ tag.toUpperCase() }}
+                  </a-tag>
+                </span>
               </a-table>
             </div>
           </a-tab-pane>
@@ -60,7 +87,16 @@
                 bordered
                 :pagination="paginationOpt"
               >
-              <a slot="number" slot-scope="text">{{ text }}</a>
+                <a slot="number" slot-scope="text">{{ text }}</a>
+                <span slot="tags" slot-scope="tags">
+                  <a-tag
+                    v-for="tag in tags"
+                    :key="tag"
+                    :color="tag === '报警' ? 'red' : 'purple'"
+                  >
+                    {{ tag.toUpperCase() }}
+                  </a-tag>
+                </span>
               </a-table>
             </div>
           </a-tab-pane>
@@ -72,33 +108,31 @@
                 bordered
                 :pagination="paginationOpt"
               >
-              <a slot="number" slot-scope="text">{{ text }}</a>
+                <a slot="number" slot-scope="text">{{ text }}</a>
               </a-table>
             </div>
           </a-tab-pane>
-
         </a-tabs>
       </div>
-
     </div>
   </div>
 </template>
 
 <script>
-import { Line,Area,Column,Liquid } from "@antv/g2plot";
+import { Line, Area, Column, Liquid } from "@antv/g2plot";
 export default {
-    created() {},
-    mounted() {
-      this.draw()
+  created() {},
+  mounted() {
+    this.draw();
   },
   data() {
     return {
       totalnum: "1563",
       recentnum: "66",
-      
-      paginationOpt: {  
-        defaultCurrent: 1,  // 默认当前页数  
-        defaultPageSize: 6,   // 默认当前页显示数据的大小  
+
+      paginationOpt: {
+        defaultCurrent: 1, // 默认当前页数
+        defaultPageSize: 6, // 默认当前页显示数据的大小
       },
 
       warnningdata: [
@@ -111,13 +145,21 @@ export default {
         { time: "2021-03-07 ", num: 28 },
       ],
 
-      totalColumns:[
+      totalColumns: [
         {
           title: "车牌号",
           dataIndex: "number",
           key: "number",
           align: "center",
-          scopedSlots: { customRender: 'number' },
+          scopedSlots: { customRender: "number" },
+        },
+        {
+          title: "等级",
+          dataIndex: "tags",
+          key: "tags",
+          align: "center",
+          
+          scopedSlots: { customRender: "tags" },
         },
         {
           title: "最后上传时间",
@@ -138,9 +180,9 @@ export default {
           align: "center",
         },
         {
-          title: "等级",
-          dataIndex: "grade",
-          key: "grade",
+          title: "报警阈值",
+          dataIndex: "section",
+          key: "section",
           align: "center",
         },
         {
@@ -150,61 +192,77 @@ export default {
           align: "center",
         },
       ],
-      totalData:[
+      totalData: [
         {
           number: "皖11111",
           updateTime: "2021-03-10 21:39",
-          location:"轮胎1",
+          location: "轮胎1",
           value: "100℃",
-          grade:"报警",
+          tags: ["报警"],
+          section:"20 ~ 80℃",
           driver: "张三",
         },
         {
           number: "皖22222",
           updateTime: "2021-03-10 21:33",
-          location:"油位",
+          location: "油位",
           value: "1%",
-          grade:"报警",
+          tags: ["报警"],
+          section:"30 ~ 100%",
           driver: "李四",
         },
         {
           number: "皖33333",
           updateTime: "2021-03-10 21:23",
-          location:"温度测量点1",
+          location: "温度测量点1",
           value: "60℃",
-          grade:"预警",
+          tags: ["预警"],
+          section:"0 ~ 20℃",
           driver: "王五",
         },
         {
           number: "皖44444",
           updateTime: "2021-03-10 22:33",
-          location:"温度测量点2",
+          location: "温度测量点2",
           value: "70℃",
-          grade:"报警",
+          tags: ["报警"],
+          section:"0 ~ 20℃",
           driver: "无名",
         },
         {
           number: "皖55555",
           updateTime: "2021-03-10 11:33",
-          location:"温度测量点3",
+          location: "温度测量点3",
           value: "72℃",
-          grade:"报警",
+          tags: ["报警"],
+          section:"0 ~ 20℃",
           driver: "小芮",
         },
         {
           number: "皖66666",
           updateTime: "2021-03-10 20:33",
-          location:"温度测量点4",
+          location: "温度测量点4",
           value: "60℃",
-          grade:"预警",
+          tags: ["预警"],
+          section:"0 ~ 20℃",
           driver: "司机",
         },
         {
           number: "皖66666",
           updateTime: "2021-03-10 20:33",
-          location:"温度测量点4",
+          location: "温度测量点4",
           value: "60℃",
-          grade:"预警",
+          tags: ["预警"],
+          section:"0 ~ 20℃",
+          driver: "司机",
+        },
+        {
+          number: "皖77777",
+          updateTime: "2021-03-10 20:33",
+          location: "胎温传感器1",
+          value: "100℃",
+          tags: ["报警"],
+          section:"20 ~ 80℃",
           driver: "司机",
         },
       ],
@@ -215,7 +273,7 @@ export default {
           dataIndex: "number",
           key: "number",
           align: "center",
-          scopedSlots: { customRender: 'number' },
+          scopedSlots: { customRender: "number" },
         },
         {
           title: "冷机设备号",
@@ -293,38 +351,36 @@ export default {
           temp: "9.2",
         },
       ],
-
     };
   },
   methods: {
-    draw(){
+    draw() {
       const columnPlot = new Column("warningdiagram", {
-        title:{
-          visible:true,
-          text:"近一周报警数量",
+        title: {
+          visible: true,
+          text: "近一周报警数量",
         },
         data: this.warnningdata,
-        padding: 'auto',
+        padding: "auto",
         xField: "time",
         yField: "num",
-        yAxis:{
-          min:0,
-          max:30
+        yAxis: {
+          min: 0,
+          max: 30,
         },
         meta: {
           time: {
-            alias: ' ',
+            alias: " ",
           },
           num: {
-            alias: '报警数',
+            alias: "报警数",
           },
         },
-        columnSize:40
+        columnSize: 40,
       });
       columnPlot.render();
-    }
+    },
   },
-
 };
 </script>
 
@@ -391,7 +447,7 @@ export default {
     }
     .home-item-bottom {
       width: 100%;
-      height: 500px;
+      height: 520px;
       background: #ffffff;
       margin: 10px 10px 0px 0px;
       box-shadow: 5px 5px 5px #dad8d8;
