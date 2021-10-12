6f659d3b3303fb5231923ca1a62fbe240ccd1399
gansu <1374646309@qq.com>
2021-10-12 20:22:20 +0800

    个人信息更新

--- src/views/User/Information.vue
+++ src/views/User/Information.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <div>
-      <a-row
+      <el-row
         :grid="{ gutter: 16, xs: 2, sm: 2, md: 2, lg: 2, xl: 2, xxl: 2 }"
-        :data-source="data"
+        
       >
-        <a-col :span="5" style="margin-right: 20px">
+        <el-col :span="5" style="margin-right: 20px">
           <a-card hoverable style="text-align: center; height: 960px">
             <br />
             <br />
@@ -14,47 +14,48 @@
             <a-avatar :size="80" icon="user" />
             <br />
             <br />
-            <a-card-meta title="赵某某" description="海纳百川，有容乃大">
+            <a-card-meta
+              :title="this.username"
+              description="海纳百川，有容乃大"
+            >
             </a-card-meta>
             <br />
             <br />
             <p><a-icon type="user" />职位：经理</p>
-            <p><a-icon type="apartment" />公司：弘恩科技</p>
-            <p><a-icon type="phone" />电话：13487882144</p>
-            <p><a-icon type="home" />家庭住址：安徽省合肥市</p>
+            <p><a-icon type="phone" />手机：{{ this.phone }}</p>
+            <p><a-icon type="mail" />邮箱：{{ this.email }}</p>
+            <p><a-icon type="home" />家庭住址：{{ this.userAddress }}</p>
           </a-card>
-        </a-col>
-        <a-col :span="18">
+        </el-col>
+        <el-col :span="18">
           <a-card title="总资产" style="height: 960px" hoverable>
             <div id="chartPie" style="height: 200px; width: 500px"></div>
-
             <p>2021/3/8<span class="divcss5-right"> 每天00：00更新</span></p>
-
             <div>
-              <a-row>
-                <a-col :span="8">
+              <el-row>
+                <el-col :span="8">
                   <a-card title="日均里程" :bordered="true" class="quanju">
                     <div id="main1" style="height: 200px"></div>
                   </a-card>
-                </a-col>
-                <a-col :span="8">
+                </el-col>
+                <el-col :span="8">
                   <a-card title="日均行驶时长" :bordered="true" class="quanju">
                     <div id="main2" style="height: 200px"></div>
                   </a-card>
-                </a-col>
-                <a-col :span="8">
+                </el-col>
+                <el-col :span="8">
                   <a-card title="日均时速" :bordered="true" class="quanju">
                     <div id="main3" style="height: 200px"></div>
                   </a-card>
-                </a-col>
-              </a-row>
-              <a-row>
-                <a-col :span="8">
+                </el-col>
+              </el-row>
+              <el-row>
+                <el-col :span="8">
                   <a-card title="日耗油" :bordered="true" class="quanju">
                     <div id="main4" style="height: 200px"></div>
                   </a-card>
-                </a-col>
-                <a-col :span="8">
+                </el-col>
+                <el-col :span="8">
                   <a-card
                     title="平均体积装载率"
                     :bordered="true"
@@ -62,8 +63,8 @@
                   >
                     <div id="main5" style="height: 200px"></div>
                   </a-card>
-                </a-col>
-                <a-col :span="8">
+                </el-col>
+                <el-col :span="8">
                   <a-card
                     title="平均重量装载率"
                     :bordered="true"
@@ -71,23 +72,27 @@
                   >
                     <div id="main6" style="height: 200px"></div>
                   </a-card>
-                </a-col>
-              </a-row>
+                </el-col>
+              </el-row>
             </div>
           </a-card>
-        </a-col>
-      </a-row>
+        </el-col>
+      </el-row>
     </div>
   </div>
 </template>
 <script>
-import echarts from "echarts";
 import { Area } from "@antv/g2plot";
 import { Line } from "@antv/g2plot";
 import { Pie } from "@antv/g2plot";
+import { person } from "@/api/user";
 export default {
   data() {
     return {
+      email: "",
+      phone: "",
+      username: "",
+      userAddress: "",
       data0: [
         { type: "闲置中", value: 27 },
         { type: "维修中", value: 25 },
@@ -151,206 +156,220 @@ export default {
       ],
     };
   },
-  methods: {},
-  mounted() {
-    const linePlot = new Line("main1", {
-      data: this.data1,
-      padding: "auto",
-      color: ["rgb(19, 194, 194)"],
-      label: {},
-      xField: "time",
-      yField: "num",
-      yAxis: {
-        min: 0,
-        max: 150,
-      },
-      tooltip: { showMarkers: false },
-      point: {
-        size: 5,
-        shape: "diamond",
-        style: {
-          fill: "white",
-          stroke: "#5B8FF9",
-          lineWidth: 2,
-        },
-      },
-
-      meta: {
-        time: {
-          alias: " ",
-        },
-        num: {
-          alias: "km",
-        },
-      },
-    });
-
-    linePlot.render();
-    fetch(
-      "https://gw.alipayobjects.com/os/bmw-prod/360c3eae-0c73-46f0-a982-4746a6095010.json"
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        const line = new Line("main2", {
-          data: this.data2,
-          color: ["rgb(47, 84, 235)"],
-          // color: ["#91CC75"],
-          xField: "time",
-          yField: "时长",
-          yAxis: {
-            min: 0,
-            max: 24,
-          },
-          meta: {
-            time: {
-              alias: " ",
-            },
-            时长: {
-              alias: "h",
-            },
-          },
-        });
-
-        line.render();
-      });
-
-    fetch(
-      "https://gw.alipayobjects.com/os/bmw-prod/360c3eae-0c73-46f0-a982-4746a6095010.json"
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        const line = new Line("main3", {
-          data: this.data3,
-          // color: ["rgb(250, 219, 20)"],
-          color: ["#91CC75"],
-
-          xField: "time",
-          yField: "num",
-          yAxis: {
-            min: 0,
-            max: 80,
-          },
-          meta: {
-            time: {
-              alias: " ",
-            },
-            num: {
-              alias: "km/h",
-            },
-          },
-        });
-
-        line.render();
-      });
-
-    fetch(
-      "https://gw.alipayobjects.com/os/bmw-prod/360c3eae-0c73-46f0-a982-4746a6095010.json"
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        const area = new Area("main4", {
-          // color: ["#91CC75"],
-          color: ["rgb(19, 194, 194)"],
-          data: this.data4,
-          xField: "time",
-          yField: "耗油",
-          yAxis: {
-            min: 0,
-            max: 40,
-          },
-          meta: {
-            time: {
-              alias: " ",
-            },
-            耗油: {
-              alias: "L",
-            },
-          },
-        });
-        area.render();
-      });
-    fetch(
-      "https://gw.alipayobjects.com/os/bmw-prod/360c3eae-0c73-46f0-a982-4746a6095010.json"
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        const area = new Area("main5", {
-          color: ["rgb(47, 84, 235)"],
-          data: this.data5,
-          xField: "time",
-          yField: "num",
-          yAxis: {
-            min: 0,
-            max: 100,
-          },
-          meta: {
-            time: {
-              alias: " ",
-            },
-            num: {
-              alias: "%",
-            },
-          },
-        });
-        area.render();
-      });
-    fetch(
-      "https://gw.alipayobjects.com/os/bmw-prod/360c3eae-0c73-46f0-a982-4746a6095010.json"
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        const area = new Area("main6", {
-          color: ["#91CC75"],
-          data: this.data6,
-          xField: "time",
-          yField: "num",
-          yAxis: {
-            min: 0,
-            max: 100,
-          },
-          meta: {
-            time: {
-              alias: " ",
-            },
-            num: {
-              alias: "%",
-            },
-          },
-        });
-        area.render();
-      });
-
-    const piePlot = new Pie("chartPie", {
-      appendPadding: 14,
-      data: this.data0,
-      angleField: "value",
-      colorField: "type",
-      radius: 1,
-      innerRadius: 0.6,
-      label: {
-        type: "inner",
-        offset: "-20%",
-        content: "{value}",
-        style: {
-          textAlign: "center",
-          fontSize: 10,
-        },
-      },
-      interactions: [{ type: "element-selected" }, { type: "element-active" }],
-      statistic: {
-        title: false,
-        content: {
+  created() {
+    this.getInfo();
+  },
+  methods: {
+    async getInfo() {
+      const res = await person();
+      console.log(res);
+      if (res.code == 200) {
+        this.email = res.data.email;
+        this.username = res.data.username;
+        this.phone = res.data.phone;
+        this.userAddress =
+          res.data.userAddress.province +
+          res.data.userAddress.city +
+          res.data.userAddress.county +
+          res.data.userAddress.detail;
+      }
+      console.log(this.email);
+    },
+    // 饼状图
+    tu0() {
+      const piePlot = new Pie("chartPie", {
+        appendPadding: 14,
+        data: this.data0,
+        angleField: "value",
+        colorField: "type",
+        radius: 1,
+        innerRadius: 0.6,
+        label: {
+          type: "inner",
+          offset: "-20%",
+          content: "{value}",
           style: {
-            whiteSpace: "pre-wrap",
-            overflow: "hidden",
-            textOverflow: "ellipsis",
+            textAlign: "center",
+            fontSize: 10,
           },
-          formatter: () => " ",
         },
-      },
-    });
+        interactions: [
+          { type: "element-selected" },
+          { type: "element-active" },
+        ],
+        statistic: {
+          title: false,
+          content: {
+            style: {
+              whiteSpace: "pre-wrap",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+            },
+            formatter: () => " ",
+          },
+        },
+      });
+      piePlot.render();
+    },
+    // 日均里程
+    tu1() {
+      const linePlot = new Line("main1", {
+        data: this.data1,
+        padding: "auto",
+        color: ["rgb(19, 194, 194)"],
+        label: {},
+        xField: "time",
+        yField: "num",
+        yAxis: {
+          min: 0,
+          max: 150,
+        },
+        tooltip: { showMarkers: false },
+        point: {
+          size: 5,
+          shape: "diamond",
+          style: {
+            fill: "white",
+            stroke: "#5B8FF9",
+            lineWidth: 2,
+          },
+        },
+        meta: {
+          time: {
+            alias: " ",
+          },
+          num: {
+            alias: "km",
+          },
+        },
+      });
+      linePlot.render();
+    },
+    // 日均行驶时长
+    tu2() {
+      const line = new Line("main2", {
+        data: this.data2,
+        color: ["rgb(47, 84, 235)"],
+        // color: ["#91CC75"],
+        xField: "time",
+        yField: "时长",
+        yAxis: {
+          min: 0,
+          max: 24,
+        },
+        meta: {
+          time: {
+            alias: " ",
+          },
+          时长: {
+            alias: "h",
+          },
+        },
+      });
 
-    piePlot.render();
+      line.render();
+    },
+    // 日均时速
+    tu3() {
+      const line = new Line("main3", {
+        data: this.data3,
+        // color: ["rgb(250, 219, 20)"],
+        color: ["#91CC75"],
+
+        xField: "time",
+        yField: "num",
+        yAxis: {
+          min: 0,
+          max: 80,
+        },
+        meta: {
+          time: {
+            alias: " ",
+          },
+          num: {
+            alias: "km/h",
+          },
+        },
+      });
+
+      line.render();
+    },
+    // 日耗油
+    tu4() {
+      const area = new Area("main4", {
+        // color: ["#91CC75"],
+        color: ["rgb(19, 194, 194)"],
+        data: this.data4,
+        xField: "time",
+        yField: "耗油",
+        yAxis: {
+          min: 0,
+          max: 40,
+        },
+        meta: {
+          time: {
+            alias: " ",
+          },
+          耗油: {
+            alias: "L",
+          },
+        },
+      });
+      area.render();
+    },
+    // 体积装载率
+    tu5() {
+      const area = new Area("main5", {
+        color: ["rgb(47, 84, 235)"],
+        data: this.data5,
+        xField: "time",
+        yField: "num",
+        yAxis: {
+          min: 0,
+          max: 100,
+        },
+        meta: {
+          time: {
+            alias: " ",
+          },
+          num: {
+            alias: "%",
+          },
+        },
+      });
+      area.render();
+    },
+    // 重量装载率
+    tu6() {
+      const area = new Area("main6", {
+        color: ["#91CC75"],
+        data: this.data6,
+        xField: "time",
+        yField: "num",
+        yAxis: {
+          min: 0,
+          max: 100,
+        },
+        meta: {
+          time: {
+            alias: " ",
+          },
+          num: {
+            alias: "%",
+          },
+        },
+      });
+      area.render();
+    },
+  },
+  mounted() {
+    this.tu0(),
+      this.tu1(),
+      this.tu2(),
+      this.tu3(),
+      this.tu4(),
+      this.tu5(),
+      this.tu6();
   },
 };
 </script>
